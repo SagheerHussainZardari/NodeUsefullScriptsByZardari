1bfaca984e7159e334bdca380da1468c637ae861
Sagheer Hussain Zardari <sagheerhzardari@gmail.com>
2022-11-25 16:33:42 +0500

    removed depricated function

--- node_create.sh
+++ node_create.sh
@@ -203,12 +203,12 @@ export default new AuthService();' > src/services/auth.service.js
 
 echo 'class ResponseService {
     sendResult(res,data,message,type,status){
-        res.send({
+        res.status(status).send({
             result: data ?? [],
             error: type == "error" ? message : null,
             message: type == "success" ? message : null,
             status: status
-        },status)
+        })
     }
 }
 
