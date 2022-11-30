76f11291e6eae274b77001847f104f2cbfdfc793
Sagheer Hussain Zardari <sagheer@medusadistribution.com>
2022-11-30 17:56:10 +0500

    added Token Invalid

--- node_create.sh
+++ node_create.sh
@@ -241,7 +241,7 @@ import {sendResult} from "../helpers/utils.js";
 const verifyToken = (req,res,next) => {
       Jwt.verify(req.headers.authorization ? req.headers.authorization.split(" ")[1] : "",process.env.JWT_SECRET,(err,result)=>{
             if(err){
-                logger.error("Token InValid: "+req.headers.authorization.split(" ")[1])
+                logger.error("Token InValid");
                 return  sendResult(res,[],"UnAuthorized","error",403)
             }
 
